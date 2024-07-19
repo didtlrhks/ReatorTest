e093cacb9498754bc6287f2f715154f6f68b5db1
sikwan <yysskk99999@naver.com>
2024-07-19 19:57:59 +0900

    init

---
+++ ReactorTest/ReactorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactorTestApp.swift
+//  ReactorTest
+//
+//  Created by 양시관 on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ReactorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
