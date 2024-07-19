d6546ccfba2dd48c61e1fca6601fa3933efe6a33
sikwan <yysskk99999@naver.com>
2024-07-19 21:55:04 +0900

    finish add

--- ReactorTest/TestView.swift
+++ ReactorTest/TestView.swift
@@ -6,6 +6,7 @@
 //
 import SwiftUI
 
+
 struct TestView: View {
     @State private var backgroundColor: Color = .blue
     @State private var firstTimer: Timer?
@@ -27,14 +28,19 @@ struct TestView: View {
                 }
             
             VStack {
-                Text("테스트 화면")
+                Text("색이 변하면 화면을 터치하세요!")
                     .font(.largeTitle)
+                    .multilineTextAlignment(.center)
                     .padding()
                 Spacer()
                 
                 if timeElapsed > 0 {
                     Text("반응 시간: \(String(format: "%.5f", timeElapsed)) 초")
                         .font(.title)
+                        .padding()
+                    
+                    Text("\(getReactionTimePercentile(reactionTime: timeElapsed))")
+                        .font(.title2)
                         .padding()
                 }
             }
@@ -92,5 +98,25 @@ struct TestView: View {
         var records = UserDefaults.standard.array(forKey: "reactionTimes") as? [Double] ?? []
         records.append(time)
         UserDefaults.standard.set(records, forKey: "reactionTimes")
+    }
+    func getReactionTimePercentile(reactionTime: Double) -> String {
+        switch reactionTime {
+        case 0..<0.1:
+            return "Top 1%"
+        case 0.1..<0.15:
+            return "Top 5%"
+        case 0.15..<0.2:
+            return "Top 10%"
+        case 0.2..<0.25:
+            return "Top 25%"
+        case 0.25..<0.3:
+            return "Top 50%"
+        case 0.3..<0.35:
+            return "Top (50-75%)"
+        case 0.35..<0.4:
+            return "Top (75-90%)"
+        default:
+            return "굉장히 느려요."
+        }
     }
 }
